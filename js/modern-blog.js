8d73fd60704d437aa3cb3dca12220ed619af59cb
saurabhlondhe <saurabhlondhe1111@gmail.com>
2019-07-03 21:16:31 +0530

    added animations to image popup

--- js/modern-blog.js
+++ js/modern-blog.js
@@ -360,14 +360,35 @@ window.addEventListener("load",function(){
 },false);
 
 var i,j;
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
 var card=document.getElementsByClassName("card__content");
 for (i = 0; i < card.length; ++i) {
     var t=card[i].getElementsByTagName("img");
     for (j = 0; j < t.length; ++j) {
         if (t[j].className !="meta__avatar"){
             t[j].style.cursor="pointer";
-            t[j].setAttribute("onclick","window.open('"+t[j].src+"');");
+            t[j].onclick = function(){
+                modal.style.display = "block";
+                modalImg.src = this.src;
+                captionText.innerHTML = this.alt;
+                modalImg.setAttribute("onclick","window.open('"+this.src+"');");
+                modalImg.style.cursor="pointer";
+              }
+              
         }
     }
 
 }
+
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+modal.onclick = function() { 
+    modal.style.display = "none";
+  }
